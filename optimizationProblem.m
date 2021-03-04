3e43411536c67df8a6ca7bb9b1e85c4940c80421
Filip Szczepankiewicz <filip.szczepankiewicz@gmail.com>
2021-03-04 13:55:58 +0100

    Update optimizationProblem.m
    Added specific behavior for specific and general optimization
    Updated calculation of sign vector to have reversal in the middle of pause time.

--- optimizationProblem.m
+++ optimizationProblem.m
@@ -37,7 +37,8 @@ classdef optimizationProblem
         MaxFunEval = 1e5;
         MaxIter    = 5e3;
         motionCompensation = struct('order', [], 'maxMagnitude', [], 'linear', [])
-        doBackgroundCompensation = false
+        doBackgroundCompensation = 2; % 0 = off; 1 = general timing cond.; 2 = specific timing cond.
+        startTime = 0; % Time from the excitataion (t=0) to the first gradietn waveform sample in ms.
     end
     
     properties (SetAccess = private)
@@ -78,31 +79,39 @@ classdef optimizationProblem
             obj.gMaxConstraint = obj.gMax*obj.dt;
             obj.sMaxConstraint = obj.sMax*obj.dt^2;
             obj.integralConstraint = obj.eta*obj.gMaxConstraint^2*obj.totalTimeActual/obj.dt;
+            obj.tolMaxwell = obj.MaxwellIndex/obj.dt;
             
-            %% Maxwell compensation
-            obj.tolMaxwell = obj.MaxwellIndex/obj.dt; %
-            
-            if ~isempty(obj.zeroGradientAtIndex) && (obj.doMaxwellComp || obj.doBackgroundCompensation)
-                signs = ones(obj.N - 1,1); % Ghost points excluded during opt
-                signs(obj.zeroGradientAtIndex(end) + 1:end) = -1;
-                obj.signs = signs;
-                
-            else
-                % Maxwell terms cannot be compensated if no 180 pulses are
-                % used. Normally this kind of optimization is intended for
-                % a repetition of two identical self-balanced waveforms,
-                % but it may be necessary to warn users that single-sided
-                % experiments will always incurr some error due to
-                % concomitant fields. In practice the "weight" of the
-                % optimization with respect to Maxwell terms is removed by
-                % setting the sign vector to all zeros.
-                obj.doMaxwellComp = false;
-                obj.signs = zeros(obj.N - 1,1)+eps; % setting to zero flips out due to sqrt(0)=complex (??)
+            % Turn Maxwell compensation off if requested
+            % Can also be written as a more confusing and compact form:
+            % obj.tolMaxwell = obj.MaxwellIndex/obj.dt / (obj.doMaxwell>0);
+            % But doMaxwell can also be entirely replaced by
+            % obj.MaxwellIndex where < inf indicates that its ON but this will
+            % complicate the GUI.
+            if ~obj.doMaxwellComp
+                obj.tolMaxwell = inf;
             end
+            
+            
+            %% Create spin dephasing direction vector
+            if ~isempty(obj.zeroGradientAtIndex)
+                signs = ones(obj.N - 1,1); % Ghost points excluded during opt
+                
+                % Assume that sign change happens in the middle of the pause
+                mi = median(obj.zeroGradientAtIndex);
+                
+                signs(round(mi):end) = -1;
+                
+                if (mi==round(mi))
+                    signs(round(mi)) = 0;
+                end
+                
+                obj.signs = signs;
+            end
+            
             
             %% Motion compensation
             if length(obj.motionCompensation.maxMagnitude) ~= length(obj.motionCompensation.order)
-                error('motionCompensation.maxMagnitude must have the same size as motionCompensation.order.')                
+                error('motionCompensation.maxMagnitude must have the same size as motionCompensation.order.')
             end
             
             if isempty(obj.motionCompensation.maxMagnitude)
@@ -112,6 +121,52 @@ classdef optimizationProblem
                 % motionCompensation.maxMagnitude
                 obj.motionCompensation.linear = (obj.motionCompensation.maxMagnitude <= 0);
             end
+            
+            %% Cross-term-compensation
+            switch obj.doBackgroundCompensation
+                case 0 % No compensation
+                    % Do nothing
+                    
+                case 1 % General timing condition
+                    % This case requires velocity compensation.
+                    
+                    ind_velo = find(obj.motionCompensation.order==1, 1);
+                    
+                    % Check if velocity compensation is requested at all.
+                    % If not, force linear constraint.
+                    % Else, check that requested velo compensation is
+                    % linear. If not, throw error since instructions cannot
+                    % be fulfilled.
+                    
+                    if isempty(ind_velo)
+                        ind = numel(obj.motionCompensation.order)+1;
+                        obj.motionCompensation.order(ind) = 1;
+                        obj.motionCompensation.linear(ind) = 1;
+                        obj.motionCompensation.maxMagnitude(ind) = 0;
+                    else
+                        if ~obj.motionCompensation.linear(ind_velo)
+                            error('Cross-term-compensation for a general timing requires velocity compensation!')
+                        end
+                    end
+                    
+                case 2 % Specific timing condition
+                    % Specific timing condition requires that the start
+                    % time is set. The start time can be zero, but it is
+                    % unlikely that this is an accurate setup, so we throw
+                    % a warning.
+                    
+                    if obj.startTime == 0
+                        warning('Start time for waveform is t = 0, which is an unlikely setting. Please check!')
+                    end
+                    
+                    if obj.startTime < 0
+                        error('Start time cannot be smaller than zero!')
+                    end
+                    
+                otherwise
+                    error('Selection for Cross-term-compensation not recognized! Use value 0, 1 or 2.')
+            end
+            
         end
     end
 end
